ed19f7b510cc3c710d1204c9cc7ac8ff669b8b2c
root <emmanuel.utrum@pm.me>
2018-09-29 06:44:30 +0000

    added container name parameter

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-( cd ../ && docker run --rm -ti --net=host -v=`pwd`/conf:/hodl-api/conf kmdplatform/hodl-api )
+( cd ../ && docker run \
+  --rm -ti \
+  --name=hodl_api \
+  --net=host \
+  -v=`pwd`/conf:/hodl-api/conf \
+  kmdplatform/hodl-api )
 
