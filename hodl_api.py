11fad248e9c169fa5e977324b5fdf33a7f2ce315
Undefined <emmanuel.utrum@pm.me>
2018-10-05 05:15:14 +0000

    removed unused data from analyze_tx function output

--- hodl_api.py
+++ hodl_api.py
@@ -11,8 +11,6 @@ from bitcoin.wallet import (
 from binascii import unhexlify
 from conf.coin import CoinParams
 
-import json
-
 bitcoin.params = bitcoin.core.coreparams = CoinParams()
 
 
@@ -120,7 +118,6 @@ def analyze_tx(tx_hex_string):
 
     # get authorized key from redeem script
     pubkey = b2x(redeem_script_array[3])
-    output['publicKey'] = pubkey
 
     # get address from authorized key
     pubkey = unhexlify(pubkey)
@@ -146,4 +143,5 @@ def analyze_tx(tx_hex_string):
 
 if __name__ == '__main__':
     output = analyze_tx('010000000195872154854064bf78e6fca1f43b67aac4aa95f440f56373e5e02fdcfa4c64c4020000006a473044022000aaae1500a73d4ecdb466e147363ca2ec5a3eb886ab4f9dca383498944f91cf02206fb0100db280cbd83e6769d7cf6dedd30b33e2f818fffe9e1a277c84d51e43300121030efdba03bfd0a6183fa9bf0d0309f0f268b2ad71ba2805c2b735e423af7d3804ffffffff0340420f000000000017a914a09cdcaacedee8a8813fee25185c8ed1b1b06d87870000000000000000656a4c6252454445454d2053435249505420303439663263303235636231373532313033306566646261303362666430613631383366613962663064303330396630663236386232616437316261323830356332623733356534323361663764333830346163a05eba05000000001976a914e33115988e5b84d5a5d5dfb633bc6ef46715282388ac00000000')
+    import json
     print(json.dumps(output))
