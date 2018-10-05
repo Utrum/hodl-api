622fa13421d291eeea5367520c47285e15c0d9a6
Undefined <emmanuel.utrum@pm.me>
2018-10-05 17:24:00 +0000

    added SubmitTX resource, which in turn calls analyze_tx api function

--- hodl_server.py
+++ hodl_server.py
@@ -4,10 +4,21 @@ from flask_restful import Api, Resource, reqparse, fields
 import hodl_api
 import requests
 import json
+import time
 
-MIN_AMOUNT = 0.01
+
+# MIN_AMOUNT = 100
+MIN_AMOUNT = 0.01  # TESTING!
+# TOLERANCE_SEC = 43200
+TOLERANCE_SEC = 60  # TESTING!
+MAX_VEST_TIME = 120
+MIN_VEST_TIME = 60
+
 MIN_AMOUNT_SAT = MIN_AMOUNT * 100000000
-
+# MAX_VEST_TIME_SEC = MAX_VEST_TIME * 86400
+# MIN_VEST_TIME_SEC = MIN_VEST_TIME * 86400
+MAX_VEST_TIME_SEC = 1800 # TESTING!
+MIN_VEST_TIME_SEC = 900 # TESTING!
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
@@ -96,10 +107,24 @@ class SubmitTx(Resource):
             error_msg = "couldn't analyze this transaction"
             return({'error': error_msg})
 
-        # check if it complies with minimum locked amount condition
+        # check if it complies with minimum allowed locked amount
         if analysis['lockedSatoshis'] < MIN_AMOUNT_SAT:
             error_msg = 'minimum amount is ' + str(MIN_AMOUNT)
             return({'error': error_msg})
+
+        # check if tx is not trying to get rewards for less than
+        # the minimum allowed vesting period
+        nLockTime = analysis['nLockTime']
+        now = int(time.time())
+        min_unlock_time = now + MIN_VEST_TIME_SEC
+        max_unlock_time = now + MAX_VEST_TIME_SEC
+        if nLockTime < (min_unlock_time - TOLERANCE_SEC):
+            return({'error': 'expired'})
+        elif nLockTime > (max_unlock_time + TOLERANCE_SEC):
+            return({'error': "you're hodling yourself out of existence!"})
+        else:
+            return({'message': 'gonna hodl'})
+
 
         return(analysis)
 
