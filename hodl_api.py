726c3ad9b5b7b7e3e8a251f7205c6f030ac65625
Undefined <emmanuel.utrum@pm.me>
2018-10-18 05:47:39 +0000

    testing no op_return on spend tx

--- hodl_api.py
+++ hodl_api.py
@@ -88,11 +88,12 @@ def spend_command(pubkey, nLockTime, prevOuts):
     unsigned_tx = CTransaction(
         [CTxIn(outpoint, redeemScript, nSequence=0) for outpoint, prevout in prevouts],
         [
-            CTxOut(sum_in - fees, address.to_scriptPubKey()),
-            CTxOut(
-                0,
-                CScript([OP_RETURN, ('DEPOSITS UNLOCKED').encode()])
-            )
+            #CTxOut(sum_in - fees, address.to_scriptPubKey()),
+            CTxOut(sum_in - fees, address.to_scriptPubKey())
+            #CTxOut(
+            #    0,
+            #    CScript([OP_RETURN, ('DEPOSITS UNLOCKED').encode()])
+            #)
         ],
         nLockTime)
 
