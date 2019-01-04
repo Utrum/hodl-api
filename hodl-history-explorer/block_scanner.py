92fd1fc58c5aa5ab29698c6a1214ad0f30d8b2ea
Patrick Hennis <patrickhennis@gmail.com>
2019-01-03 23:18:32 -0500

    added command line arg to specify height

--- hodl-history-explorer/block_scanner.py
+++ hodl-history-explorer/block_scanner.py
@@ -3,12 +3,21 @@
 import bitcoin
 import bitcoin.rpc
 from conf.coin import CoinParams
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--height", help="increase output verbosity")
+args = parser.parse_args()
 
 bitcoin.params = bitcoin.core.coreparams = CoinParams()
-
 proxy = bitcoin.rpc.Proxy(btc_conf_file=bitcoin.params.CONF_FILE)
 
 
-# get first block
-block = proxy.call('getblock', str(0))
+if args.height:
+    print("set output width to %s" % args.height)
+    block = proxy.call('getblock', str(args.height))
+else:
+    # get first block
+    block = proxy.call('getblock', str(0))
+
 print(block)
