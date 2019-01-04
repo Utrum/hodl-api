a6addd03761426b8cd526d2e9261193a35fa9ddc
Patrick Hennis <patrickhennis@gmail.com>
2019-01-04 00:43:27 -0500

    iterates through blocks and prints data

--- hodl-history-explorer/block_scanner.py
+++ hodl-history-explorer/block_scanner.py
@@ -4,6 +4,7 @@ import bitcoin
 import bitcoin.rpc
 from conf.coin import CoinParams
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--height", help="set block height to start scan at")
@@ -14,13 +15,28 @@ proxy = bitcoin.rpc.Proxy(btc_conf_file=bitcoin.params.CONF_FILE)
 
 
 if args.height:
-    block = proxy.call('getblock', str(args.height))
+    height = args.height
 else:
-    block = proxy.call('getblock', str(0))
+    height = 0
 
-print(block['tx'])
-# print(block['height'])
+block = proxy.call('getblock', str(height))
 
-for tx in block['tx']:
-    rawtx = proxy.call('getrawtransaction', tx)
-    print(proxy.call('decoderawtransaction', rawtx))
+while True:
+    for tx in block['tx']:
+        rawtx = proxy.call('getrawtransaction', tx)
+        dtx = proxy.call('decoderawtransaction', rawtx)
+        vout = dtx['vout']
+        if len(vout) > 1:
+            asm = vout[1]['scriptPubKey']['asm']
+            if 'OP_RETURN' in asm:
+                addrs = []
+                for v in vout:
+                    if 'addresses' in v['scriptPubKey']:
+                        addrs.append(v['scriptPubKey']['addresses'])
+                data = {'txid': tx, 'height': block['height'], 'addresses': addrs}
+                print(data)
+    if 'nextblockhash' in block:
+        height = int(height) + 1
+        block = proxy.call('getblock', str(height))
+    else:
+        time.sleep(60)
