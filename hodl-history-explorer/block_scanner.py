b19a5191ba410caf11af0a0becec581320cdde2d
Patrick Hennis <patrickhennis@gmail.com>
2019-01-04 12:33:06 -0500

    fixed logic, does not crash at last block

--- hodl-history-explorer/block_scanner.py
+++ hodl-history-explorer/block_scanner.py
@@ -22,27 +22,27 @@ else:
 block = proxy.call('getblock', str(height))
 
 while True:
-    for tx in block['tx']:
-        rawtx = proxy.call('getrawtransaction', tx)
-        dtx = proxy.call('decoderawtransaction', rawtx)
-        vout = dtx['vout']
-        if len(vout) > 1:
-            asm = vout[1]['scriptPubKey']['asm']
-            if 'OP_RETURN' in asm:
-                hex = asm[10:]
-                try:
-                    asmd = bytes.fromhex(hex).decode('ascii')
-                    if 'REDEEM SCRIPT' in asmd:
-                        addrs = []
-                        for v in vout:
-                            if 'addresses' in v['scriptPubKey']:
-                                addrs.append(v['scriptPubKey']['addresses'])
-                        data = {'txid': tx, 'height': block['height'], 'addresses': addrs}
-                        print(data)
-                except Exception as e:
-                    # print(str(e))
-                    pass
     if 'nextblockhash' in block:
+        for tx in block['tx']:
+            rawtx = proxy.call('getrawtransaction', tx)
+            dtx = proxy.call('decoderawtransaction', rawtx)
+            vout = dtx['vout']
+            if len(vout) > 1:
+                asm = vout[1]['scriptPubKey']['asm']
+                if 'OP_RETURN' in asm:
+                    hex = asm[10:]
+                    try:
+                        asmd = bytes.fromhex(hex).decode('ascii')
+                        if 'REDEEM SCRIPT' in asmd:
+                            addrs = []
+                            for v in vout:
+                                if 'addresses' in v['scriptPubKey']:
+                                    addrs.append(v['scriptPubKey']['addresses'])
+                            data = {'txid': tx, 'height': block['height'], 'addresses': addrs}
+                            print(data)
+                    except Exception as e:
+                        # print(str(e))
+                        pass
         height = int(height) + 1
         block = proxy.call('getblock', str(height))
     else:
